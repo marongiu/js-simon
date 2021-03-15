f944af34d3a637baabac6389be76d14b4343a9f6
Andrea <andreamarongiu05@gmail.com>
2021-03-15 17:17:09 +0100

    Creo un prompt da ripetere 5 volte

--- main.js
+++ main.js
@@ -7,20 +7,33 @@
 // Stampo 5 numeri casuali
 $(document).ready(function() {
   // Creo un array vuota dove pushare i numeri
-    var array = [];
+  var array = [];
 
-// Creo un ciclo for per ripetere la generazione di numeri 5 volte
-    for (var i = 1; i <= 5 ; i++) {
-      var randomNumber = Math.floor(Math.random() * 150);
-      // Pusho il numero generato nell'array vuota
-      array.push(randomNumber);
+  // Creo un ciclo for per ripetere la generazione di numeri 5 volte
+  for (var i = 1; i <= 5 ; i++) {
+    var randomNumber = Math.floor(Math.random() * 150);
+    // Pusho il numero generato nell'array vuota
+    array.push(randomNumber);
+  }
+  console.log(array);
+
+  // Stampo i numeri generati
+  for (var i = 0; i < array.length; i++) {
+    document.getElementById('number').innerHTML += '<span>' + array[i] + '</span>';
+  }
+
+  // Faccio partire il timer di 30 secondi
+  setTimeout(clock, 1000);
+
+// Creo la funzione clock dove creo i miei 5 prompt per chiedere i numeri
+  function clock() {
+    //Creo la mia array vuota dove pushare i numeri dell'utente
+    var arrayUser = [];
+    for (var i = 1; i <= 5; i++) {
+      var userNumber = parseInt(prompt("Dimmi i numeri che hai visualizzato"));
+      // Li pusho nell'array vuota
+      arrayUser.push(userNumber);
     }
-
-// Stampo i numeri generati
-    for (var i = 0; i < array.length; i++) {
-      document.getElementById('number').innerHTML += '<span>' + array[i] + '</span>';
-    }
-
-    
-
+    console.log(arrayUser);
+  }
 });
